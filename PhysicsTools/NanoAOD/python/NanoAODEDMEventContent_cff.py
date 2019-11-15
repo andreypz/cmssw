3ca5fb022b780c18764a0de58ad1b32cb895677f
Andrey Pozdnyakov <andreypz@gmail.com>
2019-11-15 14:56:23 +0100

    Modification to produce NanoAOD with input variables for jet energy regression

--- PhysicsTools/NanoAOD/python/NanoAODEDMEventContent_cff.py
+++ PhysicsTools/NanoAOD/python/NanoAODEDMEventContent_cff.py
@@ -11,6 +11,25 @@ NanoAODEDMEventContent = cms.PSet(
     )
 )
 
+
+NanoAODjetEventContent = cms.PSet(
+  # Data for jet-energy regression training
+    outputCommands = cms.untracked.vstring(
+      'drop *',
+      "keep nanoaodFlatTable_*jet*Table_*_*",
+      "keep nanoaodFlatTable_*Jet*Table_*_*",
+      "keep nanoaodFlatTable_EnergyRingsTable_*_*",
+      "keep nanoaodFlatTable_rhoTable_*_*",
+      "keep nanoaodFlatTable_vertexTable_*_*",
+      "keep nanoaodFlatTable_genWeightsTable_*_*",
+      "keep nanoaodFlatTable_puTable_*_*",
+      "keep nanoaodFlatTable_metTable_*_*",
+      "keep nanoaodFlatTable_genParticleTable_*_*",
+      "keep nanoaodUniqueString_nanoMetadata_*_*",
+      "drop nanoaodFlatTable_*fatJetTable_*_*",
+    )
+)
+
 NANOAODEventContent = NanoAODEDMEventContent.clone(
     compressionLevel = cms.untracked.int32(9),
     compressionAlgorithm = cms.untracked.string("LZMA"),
